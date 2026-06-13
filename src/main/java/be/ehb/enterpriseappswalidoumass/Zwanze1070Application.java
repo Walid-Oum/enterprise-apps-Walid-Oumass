c0399f12d7965f4b874b5eb04ae2f438afb95af4
Vippy11-team <Walouma@hotmail.com>
2026-06-13 20:10:19 +0200

    Initial Spring Boot project setup

---
+++ src/main/java/be/ehb/enterpriseappswalidoumass/Zwanze1070Application.java
@@ -0,0 +1,13 @@
+package be.ehb.enterpriseappswalidoumass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zwanze1070Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zwanze1070Application.class, args);
+    }
+
+}
---
+++ src/test/java/be/ehb/enterpriseappswalidoumass/Zwanze1070ApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ehb.enterpriseappswalidoumass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zwanze1070ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
