8ccf42fcc043ffcc5059dc8a88b9abdfc9049ef1
Vippy11-team <Walouma@hotmail.com>
2026-06-14 08:38:47 +0200

    Display latest events on index page

--- src/main/java/be/ehb/enterpriseappswalidoumass/controller/PageController.java
+++ src/main/java/be/ehb/enterpriseappswalidoumass/controller/PageController.java
@@ -1,5 +1,6 @@
 package be.ehb.enterpriseappswalidoumass.controller;
 
+import be.ehb.enterpriseappswalidoumass.repository.EventRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +8,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PageController {
 
+    private final EventRepository eventRepository;
+
+    public PageController(EventRepository eventRepository){
+        this.eventRepository = eventRepository;
+    }
+
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("pageTitle", "Zwanze 1070");
+        model.addAttribute("events", eventRepository.findTop10ByOrderByTimeDesc());
         return "index";
     }
+
 }
